17106024cd64b2e366b1f0eda869988aabc873e6
SinzMise <email@sinzmise.top>
2023-08-12 18:05:52 +0800

    Site updated: 2023-08-12 18:05:47

---
+++ js/fonts.js
@@ -0,0 +1,4 @@
+$webfont.load("#site-title,#site-subtitle,#site-name,#aside-content.author-info__name,#aside-content.author-info__description", "685967922a944630b0ec03f2772b0aed", "cyjianxk");
+/*$webfont.load("#id1,.class1,h1", "685967922a944630b0ec03f2772b0aed", "cyjianxk");*/
+/*．．．*/
+$webfont.draw();
